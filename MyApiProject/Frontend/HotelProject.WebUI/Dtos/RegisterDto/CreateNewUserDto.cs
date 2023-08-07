39dff13e75bca6b931d5a11b8819fd0156f25393
Canan Umaç <47716138+cananumac@users.noreply.github.com>
2023-08-08 01:30:33 +0300

    Login

---
+++ MyApiProject/Frontend/HotelProject.WebUI/Dtos/RegisterDto/CreateNewUserDto.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HotelProject.WebUI.Dtos.RegisterDto
+{
+    public class CreateNewUserDto
+    {
+        [Required(ErrorMessage ="Ad alanı gereklidir.")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Soyad alanı gereklidir.")]
+        public string Surname { get; set; }
+
+        [Required(ErrorMessage = "Kullanıcı adı alanı gereklidir.")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Mail alanı gereklidir.")]
+        public string Mail { get; set; }
+
+        [Required(ErrorMessage = "Parola alanı gereklidir.")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Parola tekrar alanı gereklidir.")]
+        [Compare("Password",ErrorMessage ="Parolalar uyuşmuyor.")]
+        public string ConfirmPassword { get; set; }
+    }
+}
