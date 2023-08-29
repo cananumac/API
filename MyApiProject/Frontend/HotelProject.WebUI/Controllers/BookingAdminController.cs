996f6d50a984ed99b450b607a5c819e63e54799c
Canan Umaç <47716138+cananumac@users.noreply.github.com>
2023-08-29 17:28:39 +0300

    Booking

---
+++ MyApiProject/Frontend/HotelProject.WebUI/Controllers/BookingAdminController.cs
@@ -0,0 +1,29 @@
+﻿using HotelProject.WebUI.Dtos.BookingDto;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace HotelProject.WebUI.Controllers
+{
+    public class BookingAdminController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public BookingAdminController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var ressponseMessage = await client.GetAsync("http://localhost:5226/api/Booking");
+            if (ressponseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await ressponseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultBookingDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
