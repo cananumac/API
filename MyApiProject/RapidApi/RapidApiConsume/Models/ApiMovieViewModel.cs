0f6e19efa5f506e6ee32f92496b6f0e2b4fa3a89
Canan Umaç <47716138+cananumac@users.noreply.github.com>
2023-09-12 18:18:07 +0300

    RapidApi

---
+++ MyApiProject/RapidApi/RapidApiConsume/Models/ApiMovieViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System.Text.Json.Serialization;
+
+namespace RapidApiConsume.Models
+{
+    public class ApiMovieViewModel
+    {
+        [JsonPropertyName("rank")]
+        public int rank { get; set; }
+
+        [JsonPropertyName("title")]
+        public string title { get; set; }
+
+        //[JsonPropertyName("description")]
+        //public string description { get; set; }
+
+        //[JsonPropertyName("image")]
+        //public string image { get; set; }
+
+        [JsonPropertyName("genre")]
+        public List<string> genre { get; set; }
+
+        //[JsonPropertyName("thumbnail")]
+        //public string thumbnail { get; set; }
+
+        [JsonPropertyName("rating")]
+        public string rating { get; set; }
+
+        //[JsonPropertyName("id")]
+        //public string id { get; set; }
+
+        //[JsonPropertyName("year")]
+        //public int year { get; set; }
+
+    }
+}
