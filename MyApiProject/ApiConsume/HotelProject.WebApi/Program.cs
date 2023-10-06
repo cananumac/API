0511b04cb419e3dc3805472006270e9f2a918071
Canan Umaç <47716138+cananumac@users.noreply.github.com>
2023-10-06 22:45:41 +0300

    Contact

--- MyApiProject/ApiConsume/HotelProject.WebApi/Program.cs
+++ MyApiProject/ApiConsume/HotelProject.WebApi/Program.cs
@@ -37,6 +37,9 @@ namespace HotelProject.WebApi
             builder.Services.AddScoped<IBookingDal, EfBookingDal>();
             builder.Services.AddScoped<IBookingService, BookingManager>();
 
+            builder.Services.AddScoped<IContactDal, EfContactDal>();
+            builder.Services.AddScoped<IContactService, ContactManager>();
+
             builder.Services.AddAutoMapper(typeof(Program));
 
 
